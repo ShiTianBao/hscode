ecbca6d279ce719ab6dbbb3415b2e8e57d9330a7
stb <1253356807@qq.com>
2018-11-20 20:09:38 +0800

    before change input to select for search

--- target/hscode/js/checklogin.js
+++
@@ -1,8 +0,0 @@
-﻿function getCookie(name)      
-    {
-       var arr = document.cookie.match(new RegExp("(^| )"+name+"=([^;]*)(;|$)"));
-        if(arr != null) return true; return false;
-    }
-    if(getCookie('tdUser')) {
-    	window.location.href = 'main.html';
-    }
